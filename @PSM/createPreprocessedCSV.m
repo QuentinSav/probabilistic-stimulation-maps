f36820994148a36a20910948a36dfe9d3058e8a8
brainstimmaps <quentin.savary@students.unibe.ch>
2022-04-03 23:49:48 +0200

    Results of maps
    Former-commit-id: f0751251acf560000a84e99c37b94e4f1e60d2b2

--- @PSM/createPreprocessedCSV.m
+++ @PSM/createPreprocessedCSV.m
@@ -1,23 +1,29 @@
 function createPreprocessedCSV(obj, voxelSize)
 
-
-disp("Reslicing voxsize = " + voxelSize);
-
-obj.util_setFilter('raw');
-
-new_folder = ['../../03_Data/reslicedVTAsLeft/', num2str(1000*voxelSize), 'um/'];
-
-if  ~exist(new_folder, 'dir')
-    mkdir(new_folder)
+if  ~exist('voxelSize', 'var')
+    voxelSize = 'original';
 
 end
 
-for k = 1:obj.data.clinical.n
-    
-    new_filename = [new_folder, num2str(k), '.nii'];
-    ea_reslice_nii(obj.data.clinical.table.filename{k}, new_filename, voxelSize, false, 0, 1);
-    obj.data.clinical.table.filename{k} = new_filename;
+disp("Reslicing voxsize = " + voxelSize);
 
+obj.util_setFilter('rounded');
+
+if ~strcmpi(voxelSize, 'original')
+    new_folder = ['../../03_Data/reslicedVTAsLeft/', num2str(1000*voxelSize), 'um/'];
+    
+    if  ~exist(new_folder, 'dir')
+        mkdir(new_folder)
+    
+    end
+    
+    for k = 1:obj.data.clinical.n
+        
+        new_filename = [new_folder, num2str(k), '.nii'];
+        ea_reslice_nii(obj.data.clinical.table.filename{k}, new_filename, voxelSize, false, 0, 1);
+        obj.data.clinical.table.filename{k} = new_filename;
+    
+    end
 end
 
 obj.exe_compileFeatures({'coord', 'indexVTAs', 'weights', 'scores'});
@@ -31,6 +37,10 @@ container = obj.map.containerTemplate;
 %ea_save_nii(container, 'container.nii');
 container_affine = container.mat;
 container_shape = size(container.img);
+
+if strcmpi('voxelSize', 'original')
+    X = logical(X);
+end
 
 save(["../../03_Data/ML_datasets/dataset_" + 1000*voxelSize + "um.mat"], "X", "y", "container_affine", "container_shape")
 
--- script_development.m
+++ script_development.m
@@ -4,35 +4,11 @@
 
 load('../../03_Data/01_Tables/multicentricTableAllImprovedOnlyRev05.mat');
 
-psm_rImp = PSM(tableMulticentric(:, :), ...
+psm = PSM(tableMulticentric, ...
     'algorithm', 'Nguyen2019', ...
     'hemisphere', 'Both', ...
     'bypassCheck', true, ...
     'centerID', 0, ...
     'mode', 'analysis');
 
-psm_rImp.compute();
-
-tableMulticentric = renamevars(tableMulticentric, 'clinicalScore', 'relativeImprovement');
-tableMulticentric = renamevars(tableMulticentric, 'efficiency', 'clinicalScore');
-
-psm_eff = PSM(tableMulticentric(:, :), ...
-    'algorithm', 'Nguyen2019', ...
-    'hemisphere', 'Both', ...
-    'bypassCheck', true, ...
-    'centerID', 0, ...
-    'mode', 'analysis');
-
-psm_eff.compute();
-
-R2_relImp = fun_evaluate_Nguyen2019_eff_vs_relImp(psm_rImp, 'relImp');
-R2_eff = fun_evaluate_Nguyen2019_eff_vs_relImp(psm_eff, 'eff');
-
-disp('R2 overlap with efficiency Sweet spot: ')
-disp(['eff  = ', num2str(R2_eff(1))]);   
-disp(['rImp = ', num2str(R2_eff(2))]);    
-
-disp('R2 overlap with relative improvement Sweet spot: ')
-disp(['eff  = ', num2str(R2_relImp(1))]);   
-disp(['rImp = ', num2str(R2_relImp(2))]);    
-
+psm.createPreprocessedCSV;
