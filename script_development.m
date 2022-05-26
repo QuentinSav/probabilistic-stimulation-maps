984adb36cecfe05b7f317f8ce257473b626d1d9e
Savary, Quentin (STUDENTS) <quentin.savary@students.unibe.ch>
2022-05-26 11:52:53 +0200

    Add env
    Former-commit-id: cd149c6fb80fb8a96a0f0b2ac8df7e854849fb46

--- script_development.m
+++ script_development.m
@@ -2,16 +2,17 @@
 
 %% Image result 1
 
-load('../../03_Data/01_Tables/multicentricTableAllImprovedOnlyRev05.mat');
+load('../../03_Data/01_Tables/multicentricTableAllImprovedOnlyRev04.mat');
 
-psm = PSM(tableMulticentric, ...
+tableMulticentric = renamevars(tableMulticentric, "relativeImprovement", "clinicalScore");
+
+psm = PSM(tableMulticentric(1:100, :), ...
     'algorithm', 'Nguyen2019', ...
-    'hemisphere', 'Both', ...
     'bypassCheck', true, ...
     'centerID', 0, ...
     'mode', 'analysis');
 
-voxsizes = [5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20]/10;
-for k = 1:length(voxsizes)
-psm.createPreprocessedCSV(voxsizes(k));
-end
+psm.compute();
+
+
+
